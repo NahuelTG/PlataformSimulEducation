a40b58201c0c76939db2c82d95fd183ec0433680
Aron6625 <aronpad123@gmail.com>
2024-12-05 18:12:16 -0400

    conexion

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -7,14 +7,14 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCCqNHo8BI54sIVDKKMzjZHSdYqXbbkHHs",
-    authDomain: "proyectotss-a9125.firebaseapp.com",
-    projectId: "proyectotss-a9125",
-    storageBucket: "proyectotss-a9125.firebasestorage.app",
-    messagingSenderId: "734484557975",
-    appId: "1:734484557975:web:57021446ea928dc70339d2",
-    measurementId: "G-VE1E5WBMQD"
-  };
+  apiKey: "AIzaSyCCqNHo8BI54sIVDKKMzjZHSdYqXbbkHHs",
+  authDomain: "proyectotss-a9125.firebaseapp.com",
+  projectId: "proyectotss-a9125",
+  storageBucket: "proyectotss-a9125.firebasestorage.app",
+  messagingSenderId: "734484557975",
+  appId: "1:734484557975:web:57021446ea928dc70339d2",
+  measurementId: "G-VE1E5WBMQD"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
